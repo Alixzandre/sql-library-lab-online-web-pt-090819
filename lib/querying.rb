52b87f0269abba7184746495cff8a8404baa3b0a
Alexander Bispo <alexbispo173@gmail.com>
2019-11-14 02:07:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -46,5 +46,5 @@ def select_character_names_and_number_of_books_they_are_in
    FROM character_books 
    JOIN characters ON characters.id = character_books.character_id
    GROUP characters.name
-   ORDER BY COUNT(name) DESC"
+   ORDER BY COUNT(name) DESC;"
 end
