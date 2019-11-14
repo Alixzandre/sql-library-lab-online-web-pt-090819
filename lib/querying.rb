f220518aabe15a35f382a6fc256928f97fe3c3b0
Alexander Bispo <alexbispo173@gmail.com>
2019-11-14 02:06:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -44,6 +44,6 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(name)
    FROM character_books 
-   JOIN chaON
+   JOIN characters ON 
    ORDER BY COUNT(name) DESC"
 end
