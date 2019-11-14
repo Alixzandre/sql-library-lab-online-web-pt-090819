604c50525b8dc0bfadb70f587e29b82c808c51d1
Alexander Bispo <alexbispo173@gmail.com>
2019-11-14 01:53:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
    FROM series 
-   JOIN books ON "
+   JOIN books ON books.series_id = series.id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
